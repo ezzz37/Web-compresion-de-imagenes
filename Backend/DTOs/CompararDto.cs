78d2845bc45ff3423192a19d36df7cc3d5002591
ezzz3 <73205104+ezzz37@users.noreply.github.com>
2025-06-02 12:33:26 -0300

    Revert "Dev"

--- Backend/DTOs/CompararDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.DTOs
-{
-    public class CompararDto
-    {
-        public int IdImagenOriginal { get; set; }
-        public int IdImagenProcesada { get; set; }
-    }
-}
--- Backend/DTOs/ImagenDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.DTOs
-{
-    public class ImagenDto
-    {
-        public int IdImagen { get; set; }
-        public string Nombre { get; set; }
-        public string DatosImagenBase64 { get; set; }
-    }
-}
